47be2dec938dccf3c1b5f0bf7ed29db7a350c5ff
AbdirahmanAbdi <abdirahmanabdi.ma@gmail.com>
2022-10-01 16:33:48 +0300

    finished

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -10,6 +10,21 @@ class CheesesController < ApplicationController
   def show
     cheese = Cheese.find_by(id: params[:id])
     render json: cheese
+    id: cheese.id,
+    name: cheese.name,
+    price: cheese.price,
+    is_best_seller: cheese.is_best_seller,
+
   end
+  
+  def show_methodcheese = Cheese.find_by(id: params[:id])
+    if cheese
+      render json: cheese,except: [:created_at, updated]
+    else 
+      render json: {error: "Cheese not found"}, status: not_found
+    end
+  end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get '/cheeses', to: 'cheeses#index'
-  get '/cheeses/:id', to: 'cheeses#show'
+
+  # get '/cheeses/:id', to: 'cheeses#show'
+
+
+
+  get '/cheeses/:id', to: 'cheese#show_method'
 end
